91e78614f998c58122c1beeb247104f4c3ba533b
Sarolus <cactussius.primus@gmail.com>
2021-08-11 14:34:19 +0200

    Fix some issues task 12

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,10 @@ class BaseModel:
                                                          '%Y-%m-%dT%H:%M:%S.%f'
                                                          )
             else:
-                self.created_at = datetime.now()
+                kwargs['created_at'] = datetime.now()
 
-            if "__class__" in kwargs:
-                del kwargs['__class__']
+            del kwargs['__class__']
 
-            if "id" in kwargs is None:
-                self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
     def __str__(self):
