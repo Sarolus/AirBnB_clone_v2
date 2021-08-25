aa9d6a436acb7954aaff5bc4b5e4d2d658bea09d
Sarolus <cactussius.primus@gmail.com>
2021-08-25 18:18:10 +0200

    Trying to fix task 0 - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i "38i\ \tlocation \/hbnb_static {\n\t\alias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+chown -hR ubuntu:ubuntu /data
+sed -i "/^\tserver_name/ a\\\n\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 service nginx restart
